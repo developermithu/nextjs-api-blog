36441f5aeb85eeb4d293db262aff3e40145bbebc
developermithu <developermithu@gmail.com>
2025-03-25 14:08:44 +0600

    fix : category search in dashboard

--- src/services/categories.js
+++ src/services/categories.js
@@ -1,6 +1,18 @@
 import axios from '@/lib/axios';
 
-export const getCategories = () => axios.get(`/api/categories`).then((res) => res.data);
+export const getCategories = async (url) => {
+    try {
+        if (url) {
+            const response = await axios.get(url);
+            return response.data; 
+        }
+        
+        const response = await axios.get('/api/categories');
+        return response.data;
+    } catch (error) {
+        throw error;
+    }
+};
 
 export const getCategory = (id) => axios.get(`/api/categories/${id}`).then((res) => res.data);
 
